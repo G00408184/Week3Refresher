31f4f1df7379693aa056dc31fd2d7ed41e8024ac
Jakub Mamala <G00408184@atu.ie>
2024-09-30 12:48:13 +0100

    Created validation for the Product class

--- src/main/java/ie/atu/week3refresher/Product.java
+++ src/main/java/ie/atu/week3refresher/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week3refresher;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @Positive(message = "Id Cannot be negative")
     private String id;
+    @NotBlank(message = "Name can't be blank")
     private String name;
+    @PositiveOrZero(message = "Price can't be negative")
     public float price;
 
 }
