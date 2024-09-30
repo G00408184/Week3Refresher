e2c9b8629be4954828fd7fe0d771f86ce8b07c08
Jakub Mamala <G00408184@atu.ie>
2024-09-30 12:41:41 +0100

    Created the Week 3 Lab

---
+++ src/main/java/ie/atu/week3refresher/Week3RefresherApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week3refresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3RefresherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week3refresher/Week3RefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3RefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
