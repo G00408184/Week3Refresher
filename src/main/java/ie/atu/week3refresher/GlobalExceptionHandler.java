62e168d8d02d60de7a1897cd2bfea96e570b2624
Jakub Mamala <G00408184@atu.ie>
2024-09-30 13:30:25 +0100

    Got the exception handler to work using hash map

--- src/main/java/ie/atu/week3refresher/GlobalExceptionHandler.java
+++ src/main/java/ie/atu/week3refresher/GlobalExceptionHandler.java
@@ -1,18 +1,29 @@
 package ie.atu.week3refresher;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     //handle Validation errors globally
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationExceptions(MethodArgumentNotValidException ex){
-        return "error occurred" + ex.getBindingResult().getFieldError().getDefaultMessage();
+    public Map<String, String>  handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()){
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        return errors;
     }
 
 }
