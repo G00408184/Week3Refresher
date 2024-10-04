c3a2a2e2af260495bbcaa87ea9a67a592dab7994
Jakub Mamala <G00408184@atu.ie>
2024-10-04 14:29:08 +0100

    fixed open feign and controller to get it working

--- src/main/java/ie/atu/week3refresher/OpenFeign.java
+++ src/main/java/ie/atu/week3refresher/OpenFeign.java
@@ -2,12 +2,13 @@ package ie.atu.week3refresher;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-@FeignClient(name = "Coms", url = "http://localhost:8081")
+@FeignClient(name = "Coms", url = "http://localhost:8081/inventory")
 public interface OpenFeign {
     @GetMapping("/GET")
     List<Product> getProduct();
-
+    @PostMapping("/add")
     String addProduct(Product product);
 }
